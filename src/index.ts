8db81a7b55832ec2ad2495e2738d9679614c4d46
Kwun Shing Wong <on9Son@Kwuns-MacBook-Pro.local>
2020-09-19 15:03:45 +0800

    Added comments

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ interface Options {
     returnPercentage: boolean; 
 }
 
+/**
+ * This function used to calculate string length deviation
+ * 
+ * @param str1  A string used for compare.
+ * @param str2  A string used for compare.
+ * @param options option to control return
+ * 
+ * @return Number or percentage of string length deviation
+ */
+
 export default (str1: string, str2: string, options?: Options) : number => {
     const lenOfStr1: number = str1.length;
     const lenOfStr2: number = str2.length;
