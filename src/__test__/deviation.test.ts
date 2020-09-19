16a5ad4c9875a86c3a22e66acc9df96958c776cc
Kwun Shing Wong <on9Son@Kwuns-MacBook-Pro.local>
2020-09-19 15:07:03 +0800

    Package.json minor fix

--- src/__test__/deviation.test.ts
+++ src/__test__/deviation.test.ts
@@ -5,17 +5,17 @@ test('Extreme case', () => {
 });
 
 test('Str1 longer than Str2 case', () => {
-    expect(deviation('ab', 'a')).toBe(0.5);
+  expect(deviation('ab', 'a')).toBe(0.5);
 });
 
 test('Str2 longer than Str1 case', () => {
-    expect(deviation('a', 'ab')).toBe(0.5);
+  expect(deviation('a', 'ab')).toBe(0.5);
 });
 
 test('Normal case', () => {
-    expect(deviation('Hello', 'Hallo')).toBe(1);
+  expect(deviation('Hello', 'Hallo')).toBe(1);
 });
 
 test('Return percentage case', () => {
-    expect(deviation('Hello', 'Hallo', {returnPercentage : true})).toBe(100);
+  expect(deviation('Hello', 'Hallo', { returnPercentage: true })).toBe(100);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
 interface Options {
-    returnPercentage: boolean; 
+  returnPercentage: boolean;
 }
 
 /**
  * This function used to calculate string length deviation
- * 
+ *
  * @param str1  A string used for compare.
  * @param str2  A string used for compare.
  * @param options option to control return
- * 
+ *
  * @return Number or percentage of string length deviation
  */
 
-export default (str1: string, str2: string, options?: Options) : number => {
-    const lenOfStr1: number = str1.length;
-    const lenOfStr2: number = str2.length;
+export default (str1: string, str2: string, options?: Options): number => {
+  const lenOfStr1: number = str1.length;
+  const lenOfStr2: number = str2.length;
 
-    let answer: number = 0;
+  let answer: number = 0;
 
-    if (lenOfStr1 === 0 && lenOfStr2 === 0) {
-        return answer;
-    } else if (lenOfStr1 >= lenOfStr2) {
-        answer = lenOfStr2 / lenOfStr1;
-    } else {
-        answer = lenOfStr1 / lenOfStr2;
-    }
-
-    if (options && options.returnPercentage) {
-        answer *= 100;
-    }
-
+  if (lenOfStr1 === 0 && lenOfStr2 === 0) {
     return answer;
+  } else if (lenOfStr1 >= lenOfStr2) {
+    answer = lenOfStr2 / lenOfStr1;
+  } else {
+    answer = lenOfStr1 / lenOfStr2;
+  }
+
+  if (options && options.returnPercentage) {
+    answer *= 100;
+  }
+
+  return answer;
 };
